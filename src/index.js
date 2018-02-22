0dee7298baf341665dbc0b922df90da2a28de0cd
Vladimir <Leamhein@hotmail.com>
2018-02-22 12:56:19 +0300

    Done 16/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var strLength = str.length,
+  confLength = bracketsConfig.length,
+  stack = []; // stack for open brackets
+  for (let i = 0; i < strLength; i++) { //check every symbol of str
+    for (let n = 0; n < confLength; n++) {
+      if (str[i] == bracketsConfig[n][0]) { //if this symbol == open bracket
+        stack.unshift(str[i]); // place it into stack
+      };
+      if (str[i] == bracketsConfig[n][1]) {
+        if (stack[0] == bracketsConfig[n][0]) { //if this symbol - closed bracket the same type as last open bracket
+          stack.shift(); //del first symbol from stack
+        };
+      };
+    };
+  };
+  if (stack.length != 0) {
+    return false; //if in the end there is any symbols in stack
+  };
+  return true; //else
+};
