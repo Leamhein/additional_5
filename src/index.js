48711283caac9163cec934cf8fdb32b13615a3e1
Vladimir Apenko <Leamhein@hotmail.com>
2018-02-25 16:22:48 +0300

    All done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ module.exports = function check(str, bracketsConfig) {
   stack = []; // stack for open brackets
   for (let i = 0; i < strLength; i++) { //check every symbol of str
     for (let n = 0; n < confLength; n++) {
-      if (str[i] == bracketsConfig[n][0]) { //if this symbol == open bracket
+      if (str[i] == bracketsConfig[n][0] && str[i] == bracketsConfig[n][1]) { //if this symbol an quote
+        if (stack[0] == bracketsConfig[n][0]) { //if another qoute in the head of the stack
+          stack.shift(); //del it
+        } else {stack.unshift(str[i])}; //else place it into stack
+      };
+      if (str[i] == bracketsConfig[n][0] && str[i] != bracketsConfig[n][1]) { //if this symbol == open bracket
         stack.unshift(str[i]); // place it into stack
       };
-      if (str[i] == bracketsConfig[n][1]) {
+      if (str[i] == bracketsConfig[n][1] && str[i] != bracketsConfig[n][0]) {
+        if (stack.length == 0) { //if stack is empty -> false
+          return false;
+        };
         if (stack[0] == bracketsConfig[n][0]) { //if this symbol - closed bracket the same type as last open bracket
           stack.shift(); //del first symbol from stack
         };
